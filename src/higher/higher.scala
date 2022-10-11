8e4a7b555c4829fd8977eee18a77342c81a05142
Davi Suga <daviciencia1@gmail.com>
2022-10-11 13:28:10 -0300

    added scala higher kinds

--- src/higher/higher.scala
+++ src/higher/higher.scala
@@ -1,16 +1,29 @@
-trait Mappable[F[_]] {
-  def map[A, B](fa: F[A])(f: A => B): F[B]
+case class Pair[A](left: A, Right: A)
+
+enum LowBinaryTree[A]:
+  case LowLeaf(value: A)
+  case LowNode(value: Pair[LowBinaryTree[A]])
+
+enum Tree[F[_], A]:
+  case Leaf(value: A)
+  case Node(value: F[Tree[F, A]])
+
+case class AnnPair[A](label: String, left: A, right: A)
+
+type RoseTree[A] = Tree[List, A]
+
+type BinaryTree[A] = Tree[Pair, A]
+
+type AnnTree[A] = Tree[AnnPair, A]
+
+val pairTree =
+  Tree.Node(Pair(Tree.Leaf(1), Tree.Node(Pair(Tree.Leaf(2), Tree.Leaf(3)))))
+
+def sumStuff(x: BinaryTree[Int]): Int = {
+  x match
+    case Tree.Leaf(x) =>
+      print(x)
+      x
+    case Tree.Node(Pair(Tree.Leaf(a), Tree.Leaf(b))) => a + b
+    case Tree.Node(Pair(l, r)) => sumStuff(l) + sumStuff(r)
 }
-
-val listMappable = new Mappable[List] {
-  override def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
-}
-
-val seqMappable = new Mappable[Seq] {
-  override def map[A, B](fa: Seq[A])(f: A => B): Seq[B] = fa.map(f)
-}
-
-def map[A, B, F[_]](fa: F[A])(f: A => B)(implicit m: Mappable[F]): F[B] = m.map(fa)(f)
-
-def main:Unit =
-  map(seqMappable, print)
